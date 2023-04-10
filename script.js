5de947fccc78b5e357e821aa30d91ddee1d11093
jaclynwalker <122503666+jaclynwalker@users.noreply.github.com>
2023-04-10 09:54:05 -0700

    update

---
+++ script.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const banner = document.querySelector('#banner');
+    const sections = document.querySelectorAll('section');
+    const sun = document.querySelector('#sun');
+    const moon = document.querySelector('#moon');
+    let mode = 'dark';
+
+    button.addEventListener('click', function() {
+        if (mode === 'dark') {
+            body.className = 'switch';
+            banner.className = 'switch';
+            button.className = 'switch';
+            sun.className = 'switch'
+            moon.className = 'switch'
+            for (const section of sections) {
+                section.className = 'switch';
+            }
+            mode = 'light';
+        } else {
+            body.removeAttribute('class');
+            banner.removeAttribute('class');
+            button.removeAttribute('class');
+            sun.removeAttribute('class');
+            moon.removeAttribute('class');
+            for (const section of sections) {
+                section.removeAttribute('class');
+            }
+            mode = 'dark'
+        }
+    })
+})()
